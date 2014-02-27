549e21a0b1638dedc788f65848388358a08454a1
Thibaut VIARD <aethaniel@sam-geek.org>
2014-02-27 16:34:34 +0100

    [FLP] Updating files needed for empty serial API to be implemented

---
+++ libraries/mbed/targets/hal/TARGET_Atmel/serial.c
@@ -0,0 +1,65 @@
+#include "device.h"
+#include "serial_api.h"
+
+void serial_init( serial_t *obj, PinName tx, PinName rx )
+{
+}
+
+void serial_free( serial_t *obj )
+{
+}
+
+void serial_baud( serial_t *obj, int baudrate )
+{
+}
+
+void serial_format( serial_t *obj, int data_bits, SerialParity parity, int stop_bits )
+{
+}
+
+void serial_irq_handler( serial_t *obj, uart_irq_handler handler, uint32_t id )
+{
+}
+
+void serial_irq_set( serial_t *obj, SerialIrq irq, uint32_t enable )
+{
+}
+
+int serial_getc( serial_t *obj )
+{
+  return 0 ;
+}
+
+void serial_putc( serial_t *obj, int c )
+{
+}
+
+int serial_readable( serial_t *obj )
+{
+  return 0 ;
+}
+
+int serial_writable( serial_t *obj )
+{
+  return 0 ;
+}
+
+void serial_clear( serial_t *obj )
+{
+}
+
+void serial_break_set( serial_t *obj )
+{
+}
+
+void serial_break_clear( serial_t *obj )
+{
+}
+
+void serial_pinout_tx( PinName tx )
+{
+}
+
+void serial_set_flow_control( serial_t *obj, FlowControl type, PinName rxflow, PinName txflow )
+{
+}
