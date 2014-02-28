8fe534d62388bd5eb1a37b2546ae86150502ec29
Thibaut VIARD <aethaniel@sam-geek.org>
2014-02-28 12:31:25 +0100

    [FLP] Completing the missing HAL draft implementations

---
+++ libraries/mbed/targets/hal/TARGET_Atmel/analogin.c
@@ -0,0 +1,15 @@
+#include "analogin_api.h"
+
+void analogin_init( analogin_t *obj, PinName pin )
+{
+}
+
+float analogin_read( analogin_t *obj )
+{
+	return 0.0f ;
+}
+
+uint16_t analogin_read_u16( analogin_t *obj )
+{
+	return 0u ;
+}
---
+++ libraries/mbed/targets/hal/TARGET_Atmel/analogout.c
@@ -0,0 +1,27 @@
+#include "analogout_api.h"
+
+void analogout_init( dac_t *obj, PinName pin )
+{
+}
+
+void analogout_free( dac_t *obj )
+{
+}
+
+void analogout_write( dac_t *obj, float value )
+{
+}
+
+void analogout_write_u16( dac_t *obj, uint16_t value )
+{
+}
+
+float analogout_read( dac_t *obj )
+{
+	return 0.0f ;
+}
+
+uint16_t analogout_read_u16 (dac_t *obj )
+{
+	return 0u ;
+}
